d41df1133f3f6b74b35f5a860606caaddb3b1a55
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-07-08 10:06:54 +0700

    step 9

--- src/util/api.js
+++ src/util/api.js
@@ -18,6 +18,11 @@ const loginApi = (email, password) => {
     return axios.post(URL_API, data)
 }
 
+const getUserApi = () => {
+    const URL_API = "/v1/api/user";
+    return axios.get(URL_API)
+}
+
 export {
-    createUserApi, loginApi
+    createUserApi, loginApi, getUserApi
 }
