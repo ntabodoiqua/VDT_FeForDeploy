38275026ac462e06bf935564f504be1a9f715b8f
Hỏi Dân IT và Eric <haryphamdev@gmail.com>
2024-07-09 13:03:59 +0700

    step 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,55 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/header";
 import axios from "./util/axios.customize"
-import { useEffect } from "react"
-
+import { useContext, useEffect } from "react"
+import { AuthContext } from "./components/context/auth.context";
+import { Spin } from "antd";
 
 function App() {
 
+  const { setAuth, appLoading, setAppLoading } = useContext(AuthContext);
+
   useEffect(() => {
-    const fetchHelloWorld = async () => {
-      const res = await axios.get(`/v1/api/`);
-      console.log(">>> check res: ", res)
+    const fetchAccount = async () => {
+      setAppLoading(true);
+      const res = await axios.get(`/v1/api/account`);
+      if (res) {
+        setAuth({
+          isAuthenticated: true,
+          user: {
+            email: res.email,
+            name: res.name
+          }
+        })
+      }
+      setAppLoading(false);
     }
 
-    fetchHelloWorld()
+    fetchAccount()
   }, [])
 
 
 
   return (
     <div>
+      {appLoading === true ?
+        <div style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)"
+        }}>
 
-      <Header />
-      <Outlet />
+          <Spin />
+
+        </div>
+        :
+        <>
+          <Header />
+          <Outlet />
+        </>
+      }
+
     </div>
   )
 }
